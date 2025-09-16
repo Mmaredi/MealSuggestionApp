cf30b4f5f92fa9de7fd13e1c33f743f5761f9e96
Mmaredi <st10500432@rcconnect.edu.za>
2025-09-16 11:46:48 +0000

    Update MainActivity.kt

--- app/src/main/java/com/example/mealsuggestionapp/MainActivity.kt
+++ app/src/main/java/com/example/mealsuggestionapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,11 +20,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MealSuggestionAppTheme {
-                uhuhuhuh
+
+                    Text("today we are going to make burger and pizza")
+                Text("meal spaghetti bolognese",)
 
 
 
             }
+            }
         }
     }
-}
+
